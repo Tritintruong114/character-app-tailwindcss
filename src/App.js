22aa09655df8c3b51ad89341099327573b9c6bfd
Tritintruong114 <truongtritin.bee@gmail.com>
2023-03-16 19:57:16 +0700

    This is for the Header and the assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-rose-600 font-light text-md sm:text-xl">
-        Character Customize
-      </h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <h3 className="text-gray-600 font-light text-xs sm:text-lg flex flex-row items-center justify-center">
+          Made by
+          <p className="font-bold px-1 text-xs sm:text-lg ">Bruno Truong</p>
+        </h3>
+      </div>
+
+      <div className="grid grid-cols-1">
+        <h1 className="text-rose-600 font-light text-lg sm:text-2xl ">
+          Avatar Creator
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
